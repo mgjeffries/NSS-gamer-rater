74a71218aca18c5b8e6292795106cf45aceb95f6
mgjeffries <gib.jeffries@gmail.com>
2020-11-02 13:45:08 -0600

    create package in models directory

---
+++ raterapp/models/__init__.py
@@ -0,0 +1,6 @@
+from .category import Category
+from .game import Game
+from .gameCategory import GameCategory
+from .gameImage import GameImage
+from .gameReview import GameReview
+from .player import Player
