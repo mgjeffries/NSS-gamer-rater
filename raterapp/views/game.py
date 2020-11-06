c0c675074f132db594b705a3de920d974ab7934b
mgjeffries <gib.jeffries@gmail.com>
2020-11-06 15:43:24 -0600

    add create method to games view

--- raterapp/views/game.py
+++ raterapp/views/game.py
@@ -10,7 +10,44 @@ from raterapp.models import Game, Player, GameCategory
 class Games(ViewSet):
     """Gamer Rater Games"""
 
-    
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized game instance
+        """
+
+        # Uses the token passed in the `Authorization` header
+        player = Player.objects.get(user=request.auth.user)
+
+        # Create a new Python instance of the Game class
+        # and set its properties from what was sent in the
+        # body of the request from the client.
+        game = Game()
+        game.player = player
+        game.title = request.data["title"]
+        game.description = request.data["description"]
+        game.designer = request.data["designer"]
+        game.year_released = request.data["year_released"]
+        game.number_of_players = request.data["number_of_players"]
+        game.time_to_play = request.data["time_to_play"]
+        game.age_recommendation = request.data["age_recommendation"]
+
+
+        # Try to save the new game to the database, then
+        # serialize the game instance as JSON, and send the
+        # JSON as a response to the client request
+        try:
+            game.save()
+            serializer = GameSerializer(game, context={'request': request})
+            return Response(serializer.data)
+
+        # If anything went wrong, catch the exception and
+        # send a response with a 400 status code to tell the
+        # client that something was wrong with its request data
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
     def list(self, request):
         """Handle GET requests to games resource
 
